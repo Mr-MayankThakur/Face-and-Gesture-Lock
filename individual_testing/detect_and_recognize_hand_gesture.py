76f5c4d31dd927f8eea677ec9543fc982f7fe35f
mayankthakur <mackv210w@gmail.com>
2020-07-22 12:44:56 +0530

    fix individual module testing

---
+++ individual_testing/detect_and_recognize_hand_gesture.py
@@ -0,0 +1,90 @@
+from modules.hand_tracking.hand_tracker import HandTracker
+from modules.gesture_recognizer.model import generate_gesture_score_fn
+from modules.io_utils.threaded_cv_input import WebcamVideoStream
+
+import cv2 as cv
+import numpy as np
+import time
+
+# gesture file path (.npy)
+gesture_path = '../modules/hand_tracking/temp/saved_gesture/gesture1_1.npy'
+saved_gesture = np.load(gesture_path, allow_pickle=True)
+
+gesture_score = generate_gesture_score_fn(saved_gesture)
+
+# initializing threaded video input
+
+palm_model_path = "../modules/hand_tracking/models/palm_detection.tflite"
+landmark_model_path = "../modules/hand_tracking/models/hand_landmark.tflite"
+anchors_path = "../modules/hand_tracking/data/anchors.csv"
+
+
+# use img_rgb
+# box_shift determines
+detector = HandTracker(palm_model_path, landmark_model_path, anchors_path,
+                       box_shift=0.2, box_enlarge=1.3)
+
+vs = WebcamVideoStream().start()
+
+frame_cnt = 0
+accum_time = 0
+curr_fps = 0
+fps = "FPS: ??"
+prev_time = time.time()
+while (True):
+    img = vs.read()
+    ret = img is not None
+
+    img_height = img.shape[0]
+    img_width = img.shape[1]
+
+    frame_cnt += 1
+    #print('-------- frame_cnt: ' + str(frame_cnt) + ' --------')
+
+    if ret:
+        img_rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+
+        try:
+            # kp is points list
+            # box is box coordinates
+            kp, box = detector(img_rgb)
+
+            # do image drawing here
+            for point in kp.astype(int):
+                cv.circle(img, tuple(point), 5, (255,0,0), thickness=-1)
+
+            #print(f"gesture score: {gesture_score(Y=kp, reflection=False)}")
+
+            if gesture_score(Y=kp, reflection=False) < 0.1:
+                print("Gesture Found")
+            else:
+                print("Wrong Gesture")
+
+        except ValueError:
+            #print("No Hands Found")
+            pass
+
+
+    curr_time = time.time()
+    exec_time = curr_time - prev_time
+    prev_time = curr_time
+    accum_time = accum_time + exec_time
+    curr_fps = curr_fps + 1
+
+    if accum_time > 1:
+        accum_time = accum_time - 1
+        fps = "FPS: " + str(curr_fps)
+        curr_fps = 0
+
+    #print(fps)
+
+    cv.imshow('img', img)
+    # vs.show('img', img)
+
+    c = cv.waitKey(1) & 0xff
+
+    if c == 27:
+        break
+
+vs.stop()
+cv.destroyAllWindows()
--- individual_testing/save_face_embeddings.py
+++ individual_testing/save_face_embeddings.py
@@ -8,11 +8,11 @@ import numpy as np
 
 def main():
     # initialise video input and Face Detector
-    face_detector = FaceDetector('modules/face_detector/data/face_detection_front.tflite')
+    face_detector = FaceDetector('../modules/face_detector/data/face_detection_front.tflite')
 
     #intitalize face recognizer
-    predictor_path = "modules/face_recognition/data/shape_predictor_5_face_landmarks.dat"
-    face_rec_model_path = "modules/face_recognition/data/dlib_face_recognition_resnet_model_v1.dat"
+    predictor_path = "../modules/face_recognition/data/shape_predictor_5_face_landmarks.dat"
+    face_rec_model_path = "../modules/face_recognition/data/dlib_face_recognition_resnet_model_v1.dat"
 
     # Load all the models we need: a shape predictor
     # to find face landmarks so we can precisely localize the face, and finally the
@@ -24,17 +24,14 @@ def main():
     # import predefined feature vector here
     # intitalize the generate_face_compare_fn with it
 
-    faces_path = 'modules/temp/faces/*.jpg'
-    vector_save_path = 'modules/temp/vectors'
+    faces_path = '../data/face_data/face_images/*.jpg'
+    vector_save_path = '../data/face_data/face_encodings'
 
     for img_path in glob.glob(faces_path):
         person_name = img_path.split('/')[-1].replace(".jpg", '')
         print(f"processing: {img_path.split('/')[-1]}")
 
         img = cv.imread(img_path)
-
-        img_height = img.shape[0]
-        img_width = img.shape[1]
 
         img_rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
 
@@ -53,8 +50,8 @@ def main():
 
 
             np.save(vector_save_path + "/" + person_name, feature_vector)
-            #cv.imwrite("vectors/" + fname + '.jpg', img)
-            cv.imshow(person_name, img)
+            cv.imwrite(vector_save_path + "/" + person_name + '.jpg', img)
+            #cv.imshow(person_name, img)
 
 if __name__ == "__main__":
     main()
