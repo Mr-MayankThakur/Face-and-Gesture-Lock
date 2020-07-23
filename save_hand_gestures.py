ba2d32f3fbecaf39d9fc3e3a6b58d12295660988
mayankthakur <mackv210w@gmail.com>
2020-07-23 16:55:53 +0530

    add automated gesture and face save

--- save_hand_gestures.py
+++ save_hand_gestures.py
@@ -1,3 +1,12 @@
+"""
+Hand Gesture Saving Script
+
+How to Use:
+1. Save a image in data/gesture_data/gesture_image/ folder containing a Hand Gesture.
+2. Run this script in the module's root directory.
+3. After processing the Gesture embeddings are saved in data/gesture_data/gesture_embeddings folder.
+"""
+
 import glob
 
 import cv2 as cv
@@ -5,32 +14,36 @@ import numpy as np
 
 from modules.hand_tracking.hand_tracker import HandTracker
 
-palm_model_path = "../modules/hand_tracking/models/palm_detection.tflite"
-landmark_model_path = "../modules/hand_tracking/models/hand_landmark.tflite"
-anchors_path = "../modules/hand_tracking/data/anchors.csv"
+palm_model_path = "./modules/hand_tracking/models/palm_detection.tflite"
+landmark_model_path = "./modules/hand_tracking/models/hand_landmark.tflite"
+anchors_path = "./modules/hand_tracking/data/anchors.csv"
 
 # use img_rgb
 # box_shift determines
 detector = HandTracker(palm_model_path, landmark_model_path, anchors_path,
                        box_shift=0.2, box_enlarge=1.3)
 
-gesture_folder_path = '../data/gesture_data/gesture_images'  # gesture images path
-gesture_save_path = '../data/gesture_data/gesture_encodings'
+gesture_folder_path = './data/gesture_data/gesture_images'  # gesture images path
+gesture_save_path = './data/gesture_data/gesture_encodings'
 
 # if verbose then the script will show images with discovered hand points
 verbose = False
 
 for file in glob.glob(gesture_folder_path + "/*.jpg"):
     file_name = file.split('/')[-1].replace(".jpg", '')
+    print(f"Processing {file_name}")
 
+    # read file for processing
     img = cv.imread(file)
 
     img_height = img.shape[0]
     img_width = img.shape[1]
 
+    # convert image to RGB for face detection since OpenCV uses BGR format.
     img_rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
 
     try:
+        # Detecting Hand then Gesture in the Image
         # kp is points list
         # box is box coordinates
         kp, box = detector(img_rgb)
@@ -46,8 +59,10 @@ for file in glob.glob(gesture_folder_path + "/*.jpg"):
         # save gesture image
         cv.imwrite(f"{gesture_save_path}/{file_name}.jpg", img)
 
+        print(f"Gesture '{file_name}' Saved.")
+
     except ValueError:
-        # print("No Hands Found")
+        print("No Hands Found")
         pass
 
     if verbose:
@@ -57,3 +72,5 @@ for file in glob.glob(gesture_folder_path + "/*.jpg"):
 
         if c == 27:
             break
+
+print("Gesture Saving Complete../nExiting...")
